e15fdaacbc61b8ca0c18728110c3b03d99721e17
daveg7lee <daveg7lee@gmail.com>
2023-06-27 09:58:00 +0900

    ✨ popup setup

--- scripts/inject.js
+++ scripts/inject.js
@@ -1,65 +1,69 @@
-badWords = ["좆병신"];
-let words = [];
+chrome.storage.local.get(["words"], async function (result) {
+  console.log(result);
+});
 
-function wait(sec) {
-  let start = Date.now(),
-    now = start;
-  while (now - start < sec * 1000) {
-    now = Date.now();
-  }
-}
+// badWords = ["좆병신"];
+// let words = [];
 
-// 가시적인 요소들에서 비속어 필터링 수행
-function filterVisibleElements() {
-  var elements = document.querySelectorAll(
-    ":not(script):not(style):not(meta):not(head):not(title)"
-  );
-  elements.forEach(function (element) {
-    filterTextNodes(element);
-  });
-  purifyBadWords();
-}
+// function wait(sec) {
+//   let start = Date.now(),
+//     now = start;
+//   while (now - start < sec * 1000) {
+//     now = Date.now();
+//   }
+// }
 
-// 텍스트 노드에서 비속어 필터링
-function filterTextNodes(element) {
-  var treeWalker = document.createTreeWalker(
-    element,
-    NodeFilter.SHOW_TEXT,
-    null,
-    false
-  );
-  var currentNode = treeWalker.nextNode();
-  while (currentNode) {
-    filterBadWords(currentNode);
-    currentNode = treeWalker.nextNode();
-  }
-}
+// // 가시적인 요소들에서 비속어 필터링 수행
+// function filterVisibleElements() {
+//   var elements = document.querySelectorAll(
+//     ":not(script):not(style):not(meta):not(head):not(title)"
+//   );
+//   elements.forEach(function (element) {
+//     filterTextNodes(element);
+//   });
+//   purifyBadWords();
+// }
 
-// 비속어 필터링 함수
-async function filterBadWords(node) {
-  badWords.map((word) => {
-    if (node.nodeValue.includes(word) && !words.includes(node)) {
-      words.push(node);
-    }
-  });
-}
+// // 텍스트 노드에서 비속어 필터링
+// function filterTextNodes(element) {
+//   var treeWalker = document.createTreeWalker(
+//     element,
+//     NodeFilter.SHOW_TEXT,
+//     null,
+//     false
+//   );
+//   var currentNode = treeWalker.nextNode();
+//   while (currentNode) {
+//     filterBadWords(currentNode);
+//     currentNode = treeWalker.nextNode();
+//   }
+// }
 
-async function purifyBadWords() {
-  await Promise.all(
-    words.map(async (word) => {
-      const text = word.nodeValue;
-      const res = await fetch("http://127.0.0.1:5000/purify", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ text }),
-      });
-      const json = await res.json();
-      word.nodeValue = json["res"];
-      wait(1);
-    })
-  );
-}
+// // 비속어 필터링 함수
+// async function filterBadWords(node) {
+//   badWords.map((word) => {
+//     if (node.nodeValue.includes(word) && !words.includes(node)) {
+//       words.push(node);
+//     }
+//   });
+// }
 
-window.addEventListener("load", filterVisibleElements);
+// async function purifyBadWords() {
+//   await Promise.all(
+//     words.map(async (word) => {
+//       const text = word.nodeValue;
+//       const res = await fetch("http://127.0.0.1:5000/purify", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ text }),
+//       });
+//       const json = await res.json();
+//       word.nodeValue = json["res"];
+//       wait(1);
+//     })
+//   );
+// }
+
+// window.addEventListener("load", filterVisibleElements);
